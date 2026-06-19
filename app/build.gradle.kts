2a7c1259f3a90c4e5ac1ed0d8166b886fff53fad
sovereignforts <sovereignforts@gmail.com>
2026-06-19 11:51:33 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,76 @@
-//
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sovereignforts.malinread"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sovereignforts.malinread"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(
+        "androidx.core:core-ktx:1.13.1"
+    )
+
+    implementation(
+        "androidx.activity:activity-compose:1.9.1"
+    )
+
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2024.06.00"
+        )
+    )
+
+    implementation(
+        "androidx.compose.ui:ui"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
+}
